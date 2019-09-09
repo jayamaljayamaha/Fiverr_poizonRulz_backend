134cb7eeaa7069ba628391d31e76542316c68ed0
yogeshadhikari <yogeshmike@gmail.com>
2019-09-09 16:59:14 +1000

    Revert "Merge pull request #2 from jayamaljayamaha/Yogesh"
    This reverts commit df573300c957b2b8459d1ea6aa5d8fe036e2fba8, reversing
    changes made to dc43827c6aa286c6a9fc838f4cc93545da4d54a5.

--- server.js
+++ server.js
@@ -1,79 +1,22 @@
 // Dependencies
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-//In-memory instance of database
-const {startDatabase} = require('./database/mongo');
-const {insertProfile, getProfile, deleteProfile, updateProfile} = require('./database/profiles');
+// MongoDB
+mongoose.connect('mongodb://localhost:27017/rest_test', {useNewUrlParser: true});
 
-// define Express app
+// Express
 const app = express();
 
-// add Helmet to enhance the API security
-app.use(helmet());
-
-// add bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-// enable Cross-Origin Resource Sharing, CORS for all requests
-app.use(cors());
+// Routes
+app.use('/api', require('./routes/api'));
+app.use('/auth', require('./routes/auth'));
 
-// add Morgan to log HTTP requests
-app.use(morgan('combined'));
-
-// define an endpoint to return all profiles
-app.get('/', async (req, res) => {
-   res.send(await getProfile());
-});
-
-// check JSON Web Tokens
-//JWT [Only checked for POST, DELETE, PUT endpoint. No authority required for GET request of this app to view]
-//JWT will intercept requests to POST, DELETE, and PUT endpoints
-const jwtCheck = jwt({
-   secret: jwksRsa.expressJwtSecret({
-      cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri:'https://dev-q39f5c5h.au.auth0.com/.well-known/jwks.json'
-   }),
-   audience: 'https://profiles-api',
-   issuer: 'https://dev-q39f5c5h.au.auth0.com/',
-   algorithms: ['RS256']
-})
-
-app.use(jwtCheck);
-
-// POST, DELETE, PUT, startDatabase
-app.post('/', async (req, res) => {
-   const newProfile = req.body;
-   await insertProfile(newProfile);
-   res.send({ message: 'New profile inserted.'});
-});
-
-app.delete('/:id', async (req, res) => {
-   await deleteProfile(req.params.id);
-   res.send({ message: 'Profile removed.' });
-});
-
-app.put('/:id', async (req, res) => {
-   const updatedProfile = req.body;
-   await updateProfile(req.params.id, updatedProfile);
-   res.send({ message: 'Profile updated.' });
-});
-
-// start the in-memory MongoDB instance
-startDatabase().then(async () => {
-   await insertProfile({title: 'Hello new profile from the in-memory database'});
-   console.log('--start database object created!--')
-})
-
-// start the server and log console
+// Start Server
 app.listen(8000, () => {
-   console.log('Example app listening on port 8000!')
-});
+    console.log('Example app listening on port 8000!')
+    });
