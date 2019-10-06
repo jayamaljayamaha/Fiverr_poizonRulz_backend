c43fbce6d25d53ccd1957bf8cbc10fbc8b233ffa
unknown <jayamaljayamaha2@gmail.com>
2019-10-06 08:52:57 +0530

    added all the routes to main server.js file

--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 const express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
-const MongoClient = require("mongodb").MongoClient;
-const ObjectId = require("mongodb").ObjectID;
 var mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
+
+var project = require('./models/project');
+var user = require('./models/user');
 
 const uri = "mongodb+srv://jayamal:TBBo2cw6fz5vT4By@spectre-h7vto.mongodb.net/test?retryWrites=true&w=majority";
 
@@ -27,74 +28,144 @@ app.use(cors());
 
 // add Morgan to log HTTP requests
 app.use(morgan('combined'));
-app.use('/', require('./routes/api'));
-app.use('/user', require('./routes/user'));
-app.use('/project', require('./routes/projects'));
+// app.use('/', require('./routes/api'));
+// app.use('/user', require('./routes/user'));
+// app.use('/project', require('./routes/projects'));
 app.use(router);
-// // define an endpoint to return all profiles
-// app.get('/', async (req, res) => {
-//     res.send(await getProfile());
-// });
-//
-// //Login
-// app.post('/login', async (req, res) => {
-//     //var email = req.userEmail;
-//     //var password = req.userPassword;
-//     console.log(req.body);
-//     res.send(await getProfile());
-//     res.send({message: 'logged in - ok'});
-// })
-//
-// app.post('/register', async (req, res) => {
-//     const newProfile = req.body;
-//     const testProfile = {Name: 'Ivan', email: 'exampleEmail@email.com', password: 'spectre'};
-//     await insertProfile(newProfile);
-//     res.send({message: 'New profile inserted.'});
-// });
-//
-// // check JSON Web Tokens
-// //JWT [Only checked for POST, DELETE, PUT endpoint. No authority required for GET request of this app to view]
-// //JWT will intercept requests to POST, DELETE, and PUT endpoints
-// const jwtCheck = jwt({
-//     secret: jwksRsa.expressJwtSecret({
-//         cache: true,
-//         rateLimit: true,
-//         jwksRequestsPerMinute: 5,
-//         jwksUri: 'https://dev-q39f5c5h.au.auth0.com/.well-known/jwks.json'
-//     }),
-//     audience: 'https://profiles-api',
-//     issuer: 'https://dev-q39f5c5h.au.auth0.com/',
-//     algorithms: ['RS256']
-// })
-//
-// //app.use(jwtCheck);
-//
-// // POST, DELETE, PUT, startDatabase
-// app.post('/', async (req, res) => {
-//     const newProfile = req.body;
-//     const testProfile = {Name: 'Ivan', email: 'exampleEmail@email.com', password: 'spectre'};
-//     await insertProfile(nestProfile);
-//     res.send({message: 'New profile inserted.'});
-// });
-//
-// app.delete('/:id', async (req, res) => {
-//     await deleteProfile(req.params.id);
-//     res.send({message: 'Profile removed.'});
-// });
-//
-// app.put('/:id', async (req, res) => {
-//     const updatedProfile = req.body;
-//     await updateProfile(req.params.id, updatedProfile);
-//     res.send({message: 'Profile updated.'});
-// });
-//
-// // start the in-memory MongoDB instance
-// startDatabase().then(async () => {
-//     await insertProfile({Name: 'Jayden P (first in DB)', email: 'exampleEmail@email.com', password: 'spectre'});
-//     console.log('--start database object created!--')
-// })
 
-// start the server and log console
+//projects end point
+router.post('/project/', function (req, res) {
+
+    var tempProject = {
+        projectName: req.body.projectName,
+        likes: req.body.likes,
+        projectImage: req.body.projectImage
+    };
+    console.log(tempProject);
+    let newProject = new project(tempProject);
+    newProject.save()
+        .then(data => {
+            if (data.length < 1) {
+                return res.status(400).json({
+                    message: "Project creation failed",
+                });
+            }
+            else {
+                return res.status(200).json({
+                    message: "Project created successfully"
+                });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        })
+});
+
+router.get('/project/', function (req, res) {
+
+    project.find({})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: error
+            });
+        })
+});
+
+router.put('/project/:projectName/likes/:likeCount', function (req, res) {
+    var projectName = req.params.projectName;
+    var count = req.params.likeCount;
+    count++;
+    console.log("here")
+    console.log(projectName + " " + count)
+    project.findOneAndUpdate({projectName: projectName}, {$set: {likes: count}}).then(data => {
+        if (data.length < 1) {
+            return res.status(400).json({
+                message: "Project not found",
+            });
+        }
+        else {
+            return res.status(200).json({
+                message: "Project likes updated successfully"
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
+    })
+});
+
+
+//user end point
+router.post('/user/auth/login', function (req, res) {
+    console.log(req.body);
+    user.find({ email: req.body.email})
+        .exec()
+        .then(function(users){
+            if(users.length<1){
+                return res.status(200).json({
+                    message: "User not found"
+                });
+            }
+            if(users[0].password === req.body.password){
+                const token = jwt.sign({
+                        email: users[0].email,
+                        userId: users[0]._id
+                    },
+                    "secret",
+                    {
+                        expiresIn: "1h"
+                    });
+                return res.status(200).json({
+                    message: "Auth successful",
+                    token: token
+                });
+            }else{
+                return res.status(401).json({
+                    message: "User auth failed"
+                });
+            }
+        })
+        .catch(function(err){
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.post('/user/auth/register', function (req, res) {
+    var user1 = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    let newUser = new user(user1);
+    newUser.save()
+        .then((data) => {
+            if(data.length<1){
+                return res.status(400).json({
+                    message: "Registration Not successfull"
+                });
+            }
+            else{
+                return res.status(200).json({
+                    message: "Registration successful",
+                    token: token
+                });
+            }
+        })
+        .catch((error)=>{
+            res.status(500).json({
+                error: error
+            });
+        })
+
+});
+
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!')
     mongoose.connect(uri, {useNewUrlParser: true})
