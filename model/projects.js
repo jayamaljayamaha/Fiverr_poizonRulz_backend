86d5819a58b4c3deb2a0ad3f870b2309adc50645
unknown <jayamaljayamaha2@gmail.com>
2019-10-09 22:56:41 +0530

    added the count likes function

--- model/projects.js
+++ model/projects.js
@@ -3,6 +3,9 @@ const mongoose= require('mongoose');
 const projectsSchema = mongoose.Schema({
     id_user: { type: String, required: true },
     target_fund: { type: Number, required: true },
+    user_name: String,
+    score_comment_react: Number,
+    image_url: String,
     current_fund: { type: Number, required: true, default: 0 },
     status: { type: String, required: true }
 });
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const jwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
 const jwksRsa = require('jwks-rsa');
 const mongoose = require('mongoose');
 
@@ -28,8 +28,8 @@ mongoose.connect("mongodb+srv://Ivan:ProjectSpectre@spectre-h7vto.mongodb.net/Sp
 const app = express();
 
 // start the server and log console
-app.listen(80, () => {
-    console.log('Example app listening on port 80!')
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000!')
 });
 
 // serve FrontEnd folder
@@ -84,6 +84,17 @@ const jwtCheck = jwt({
     algorithms: ['RS256']
 })
 
+// const jwtCheck = jwt({
+//     secret: jwksRsa.expressJwtSecret({
+//         cache: true,
+//         rateLimit: true,
+//         jwksRequestsPerMinute: 5,
+//         jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+//     }),
+//     audience: authConfig.audience,
+//     issuer: `https://${authConfig.domain}/`,
+//     algorithm: ["RS256"]
+// });
 //app.use(jwtCheck);
 
 // POST, DELETE, PUT, startDatabase
@@ -112,157 +123,156 @@ startDatabase().then(async () => {
 })
 
 // start the server and log console
-app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
 
 
 // Set up Auth0 configuration
-    const authConfig = {
-        domain: "dev-q39f5c5h.au.auth0.com",
-        audience: "https://profiles-api"
-    };
+const authConfig = {
+    domain: "dev-q39f5c5h.au.auth0.com",
+    audience: "https://profiles-api"
+};
 
 // check JSON Web Tokens
 // JWT [Only checked for POST, DELETE, PUT endpoint. No authority required for GET request of this app to view]
 // JWT will intercept requests to POST, DELETE, and PUT endpoints
-    const jwtCheck = jwt({
-        secret: jwksRsa.expressJwtSecret({
-            cache: true,
-            rateLimit: true,
-            jwksRequestsPerMinute: 5,
-            jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
-        }),
-        audience: authConfig.audience,
-        issuer: `https://${authConfig.domain}/`,
-        algorithm: ["RS256"]
-    });
+
 
 // PING : Define and endpoint that must be called with an access token
-    app.get("/api/external", jwtCheck, (req, res) => {
-        res.send({
-            msg: "Your Access Token was successfully validated! Lets upload an image or update a project?",
+app.get("/api/external", jwtCheck, (req, res) => {
+    res.send({
+        msg: "Your Access Token was successfully validated! Lets upload an image or update a project?",
 
-        });
     });
+});
 
 // Upload an image into a project
-    app.post("/imageUpload", jwtCheck, (req, res) => {
+app.post("/imageUpload", jwtCheck, (req, res) => {
 
-    })
+})
 
 // Only use to blanket check jwt for all code below. Currently jwtCheck captured in each HTTP request
 //app.use(jwtCheck);
 
 // define an endpoint to return all profiles
-    app.get('/', async (req, res) => {
-        res.send(await getProfile());
-    });
+app.get('/', async (req, res) => {
+    res.send(await getProfile());
+});
 
-    app.put('/project/:projectName/likes/:likeCount', function (req, res) {
-        var projectName = req.params.projectName;
-        var count = req.params.likeCount;
-        count++;
-        console.log("here")
-        console.log(projectName + " " + count)
-        Projects.findOneAndUpdate({projectName: projectName}, {$set: {likes: count}}).then(data => {
-            if (data.length < 1) {
-                return res.status(400).json({
-                    message: "Project not found",
-                });
-            } else {
-                return res.status(200).json({
-                    message: "Project likes updated successfully"
-                });
-            }
-        }).catch(error => {
+app.get('/project/', function (req, res) {
+
+    Projects.find({})
+        .then(data => {
+            res.send(data);
+        })
+        .catch(error => {
             res.status(500).json({
                 error: error
             });
         })
-    });
+});
+
+app.put('/project/:projectId/likes/:likeCount', function (req, res) {
+    var Id = req.params.projectId;
+    var count = req.params.likeCount;
+    count++;
+    console.log("here")
+    console.log(Id + " " + count)
+    Projects.findOneAndUpdate({_id: Id}, {$set: {score_comment_react: count}}).then(data => {
+        if (data.length < 1) {
+            return res.status(400).json({
+                message: "Project not found",
+            });
+        } else {
+            console.log(data);
+            return res.status(200).json({
+                message: "Project likes updated successfully"
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            error: error
+        });
+    })
+});
 
 //Create New Project Ivan
-    app.post('/createProject', async (req, res) => {
+app.post('/createProject', async (req, res) => {
 
-        const project = new Projects({
-            id_user: req.body.id_user,
-            target_fund: req.body.target_fund,
-            current_fund: 0,
-            status: req.body.status
+    const project = new Projects({
+        id_user: req.body.id_user,
+        target_fund: req.body.target_fund,
+        current_fund: 0,
+        status: req.body.status
+    });
+
+    project.save();
+
+    Projects.findOne().sort({'_id': -1}).limit(1).then(document => {
+
+        let id = document._id;
+        console.log(id);
+        const image = new Images({
+            id_project: id,
+            url_image: req.body.image_url
         });
-
-        project.save();
-
-        Projects.findOne().sort({'_id': -1}).limit(1).then(document => {
-
-            let id = document._id;
-            console.log(id);
-            const image = new Images({
-                id_project: id,
-                url_image: req.body.image_url
-            });
-            image.save();
-            console.log("Added");
-            res.send({message: 'Project Inserted'});
-
-        });
-
-        console.log("Finish");
-
+        image.save();
+        console.log("Added");
+        res.send({message: 'Project Inserted'});
 
     });
+
+    console.log("Finish");
+
+
+});
 //Setup image
-    app.post('/imageUpload', async (req, res) => {
-        Projects.findOne().sort({'_id': -1}).limit(1).then(document => {
-            const image = new Images({
-                id_user: document._id,
-                image_url: req.body.image_url
-            });
-            image.save();
-            console.log("Added");
-            res.send({message: 'Image setup'});
+app.post('/imageUpload', async (req, res) => {
+    Projects.findOne().sort({'_id': -1}).limit(1).then(document => {
+        const image = new Images({
+            id_user: document._id,
+            image_url: req.body.image_url
         });
-
-    });
-// Create new project
-    app.post('/newProject', async (req, res) => {
-        const newProfile = req.body;
-        await insertProfile(newProfile);
-        res.send({message: 'New project inserted.'});
+        image.save();
+        console.log("Added");
+        res.send({message: 'Image setup'});
     });
 
+});
 // Create new project
-    app.post('/login', async (req, res) => {
-        console.log(req.body);
-        res.send(await getProfile());
-        res.send({message: 'logged in - ok'});
-    })
+app.post('/newProject', async (req, res) => {
+    const newProfile = req.body;
+    await insertProfile(newProfile);
+    res.send({message: 'New project inserted.'});
+});
+
+// Create new project
+app.post('/login', async (req, res) => {
+    console.log(req.body);
+    res.send(await getProfile());
+    res.send({message: 'logged in - ok'});
+})
 
 
 // POST, DELETE, PUT, startDatabase
-    app.post('/', jwtCheck, async (req, res) => {
-        const newProfile = req.body;
-        const testProfile = {Name: 'Ivan', email: 'exampleEmail@email.com', password: 'spectre'};
-        await insertProfile(testProfile);
-        res.send({message: 'New profile inserted.'});
-    });
+app.post('/', jwtCheck, async (req, res) => {
+    const newProfile = req.body;
+    const testProfile = {Name: 'Ivan', email: 'exampleEmail@email.com', password: 'spectre'};
+    await insertProfile(testProfile);
+    res.send({message: 'New profile inserted.'});
+});
 
-    app.delete('/:id', jwtCheck, async (req, res) => {
-        await deleteProfile(req.params.id);
-        res.send({message: 'Profile removed.'});
-    });
+app.delete('/:id', jwtCheck, async (req, res) => {
+    await deleteProfile(req.params.id);
+    res.send({message: 'Profile removed.'});
+});
 
-    app.put('/:id', jwtCheck, async (req, res) => {
-        const updatedProfile = req.body;
-        await updateProfile(req.params.id, updatedProfile);
-        res.send({message: 'Profile updated.'});
-    });
+app.put('/:id', jwtCheck, async (req, res) => {
+    const updatedProfile = req.body;
+    await updateProfile(req.params.id, updatedProfile);
+    res.send({message: 'Profile updated.'});
+});
 
 // start the in-memory MongoDB instance
-    startDatabase().then(async () => {
-        await insertProfile({Name: 'Jayden P (first in DB)', email: 'exampleEmail@email.com', password: 'spectre'});
-        console.log('--start database object created!--')
-    });
-
-
-//How can i get all the user details?
+startDatabase().then(async () => {
+    await insertProfile({Name: 'Jayden P (first in DB)', email: 'exampleEmail@email.com', password: 'spectre'});
+    console.log('--start database object created!--')
+});
